30fa61ea1a328049943e13145cfc4531b4bc746b
Nadine <45455716+nadinev6@users.noreply.github.com>
2025-07-04 15:43:28 +0200

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import lingo from 'lingo.dev';
+import lingoCompiler from 'lingo.dev/compiler'; 
 
 
 // https://vitejs.dev/config/
