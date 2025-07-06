f44408bd5cfbfcc7203520af24f88c7f0e572280
Nadine <45455716+nadinev6@users.noreply.github.com>
2025-07-06 13:58:35 +0200

    Remove 'mg' locale from project

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import lingoCompiler from 'lingo.dev/compiler';
 // https://vitejs.dev/config/
 export default defineConfig(() => lingoCompiler.vite({
   sourceLocale: 'en',
-  targetLocales: ['fr', 'mg'],
+  targetLocales: ['fr'],
   models: "lingo.dev",
 })({
   plugins: [
