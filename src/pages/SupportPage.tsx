ee49889ca6bb2abe9d5e1b3d0fdf08239060e92c
Nadine <45455716+nadinev6@users.noreply.github.com>
2025-07-06 18:51:13 +0200

    Updated SupportPage.tsx

--- src/pages/SupportPage.tsx
+++ src/pages/SupportPage.tsx
@@ -4,6 +4,8 @@ import { Button } from '../components/ui/button';
 import { Input } from '../components/ui/input';
 import { Label } from '../components/ui/label';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '../components/ui/card';
+import { MagicCard } from "@/registry/magicui/magic-card";
+
 
 const SupportPage: React.FC = () => {
   const [formData, setFormData] = useState({
